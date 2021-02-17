078c9033e4664494f4623ac2a9ed331ae48ba0cc
Fran Martinez <fran.martinez.torres@gmail.com>
2021-02-17 10:08:13 +0100

    segunda version

---
+++ ClaseMobile.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.mobile = void 0;
+var mobile = /** @class */ (function () {
+    function mobile(name, model, trademark, sdsize, color, is5G, cameraNumber, price) {
+        this.name = name;
+        this.model = model;
+        this.model = trademark;
+        this.sdsize = sdsize;
+        this.color = color;
+        this.is5G = is5G;
+        this.cameraNumber = cameraNumber;
+        this.price = price;
+    }
+    return mobile;
+}());
+exports.mobile = mobile;
---
+++ ClaseMobile.ts
@@ -0,0 +1,92 @@
+export class mobile
+{
+    private name:string;
+    private model:string;
+    private trademark:string;
+    private sdsize:number;
+    private color:string;
+    private is5G:boolean;
+    private cameraNumber:number;
+    private price:number;
+
+    constructor(name:string,model:string,trademark:string,sdsize:number,color:string,is5G:boolean,cameraNumber:number,price:number)
+    {
+        this.name = name;
+        this.model = model;
+        this.model = trademark;
+        this.sdsize = sdsize;
+        this.color = color;
+        this.is5G = is5G;
+        this.cameraNumber = cameraNumber;
+        this.price = price;
+    }
+    public getName():string
+    {
+        return this.name;
+    }
+    public getModel():string
+    {
+        return this.model;
+    }
+    public getTradeMark():string
+    {
+        return this.trademark;
+    }
+    public getSdsize():number
+    {
+        return this.sdsize;
+    }
+    public getColor():string
+    {
+        return this.color;
+    }
+    public getIs5g():boolean
+    {
+        return this.is5G;
+    }
+    public getCameraNumber():number
+    {
+        return this.cameraNumber;
+    }
+    public getPrice():number
+    {
+        return this.price;
+    }
+    public setName(name:string)
+    {
+        this.name = name;
+    }
+    public setModel(model:string)
+    {
+        this.model = model;
+    }
+    public setTradeMark(trademark:string)
+    {
+        this.trademark = trademark;
+    }
+    public setSdsize(sdsize:number)
+    {
+        this.sdsize = sdsize;
+    }
+    public setColor(color:string)
+    {
+        this.color = color;
+    }
+    public setIs5g(is5g:boolean)
+    {
+        this.is5G = is5g;
+    }
+    public setCameraNumber(cameraNumber:number)
+    {
+        this.cameraNumber = cameraNumber;
+    }
+    public setPrice(price:number)
+    {
+        this.price = price
+    }
+    
+
+
+
+    
+}
