535ae51a34d025841bd1b47ed1128ba741dad43e
Fran Martinez <fran.martinez.torres@gmail.com>
2021-02-17 17:20:56 +0100

    página 4 triangulo

--- mainObjeto.ts
+++ mainObjeto.ts
@@ -1,11 +1,16 @@
 import {Punto} from "./clasePunto"
+import { Triangulo } from "./claseTriangulo"
 let cordenaditas:Punto = new Punto(2,2)
 console.log(cordenaditas.toStringg())
 let otroPunto:Punto = new Punto(1,1)
 let p1:Punto = new Punto(12,3)
 let p2:Punto = new Punto(1,2)
+let p3:Punto = new Punto(5,7)
 let puntazos:Punto[] = [p1,p2]
+let triangulito:Triangulo = new Triangulo(p1,p2,p3)
+
 console.log(p1.distanciaAlOrigen())
 console.log(cordenaditas.calcularDistancia(otroPunto))
 console.log(p1.calcularMasCercano(puntazos))
+console.log(triangulito.calcularLongitudLados())
 
